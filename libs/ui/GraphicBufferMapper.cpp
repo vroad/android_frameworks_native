7c55caf00d64ece1c91d39250b301f7ca812c0b3
Erfan Abdi <erfangplus@gmail.com>
2022-03-14 23:02:05 +0000

    GraphicBufferMapper: Skip buffer validation
    Change-Id: Ia7580cec2e4c89f8de7768361ad9852374991fda

--- libs/ui/GraphicBufferMapper.cpp
+++ libs/ui/GraphicBufferMapper.cpp
@@ -64,8 +64,8 @@ GraphicBufferMapper::GraphicBufferMapper() {
 }
 
 status_t GraphicBufferMapper::importBuffer(buffer_handle_t rawHandle,
-        uint32_t width, uint32_t height, uint32_t layerCount,
-        PixelFormat format, uint64_t usage, uint32_t stride,
+        __attribute__ ((unused)) uint32_t width, __attribute__ ((unused)) uint32_t height, __attribute__ ((unused)) uint32_t layerCount,
+        __attribute__ ((unused)) PixelFormat format, __attribute__ ((unused)) uint64_t usage, __attribute__ ((unused)) uint32_t stride,
         buffer_handle_t* outHandle)
 {
     ATRACE_CALL();
@@ -77,13 +77,13 @@ status_t GraphicBufferMapper::importBuffer(buffer_handle_t rawHandle,
         return error;
     }
 
-    error = mMapper->validateBufferSize(bufferHandle, width, height, format, layerCount, usage,
+    /*error = mMapper->validateBufferSize(bufferHandle, width, height, format, layerCount, usage,
                                         stride);
     if (error != NO_ERROR) {
         ALOGE("validateBufferSize(%p) failed: %d", rawHandle, error);
         freeBuffer(bufferHandle);
         return static_cast<status_t>(error);
-    }
+    }*/
 
     *outHandle = bufferHandle;
 
